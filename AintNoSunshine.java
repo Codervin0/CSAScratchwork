e5a5bb2ca4c9a5169844c779a169e653b47e699f
Codervin0 <corvin.krause.909@gmail.com>
2026-09-23 11:48:22 -0700

    sunshine song

---
+++ AintNoSunshine.java
@@ -0,0 +1,61 @@
+public class AintNoSunshine {
+
+   public static void main(String[] args) {
+      sunshine();
+      System.out.println("It's not warm when she's away");
+      sunshine();
+      System.out.println("And she's always gone too long");
+      goesAway();
+      System.out.println();
+      System.out.println("Wonder this time where she's gone");
+      System.out.println("Wonder if she's gone to stay");
+      sunshine();
+      noHome();
+      goesAway();
+      System.out.println();
+      System.out.println("And I know, I know, I know, I know");
+      iKnow();
+      iKnow();
+      iKnow();
+      iKnow();
+      System.out.println("I know, I know");
+      System.out.println();
+      System.out.println("Hey, I ought to leave young thing alone");
+      System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
+      sunshine();
+      System.out.println("Only darkness every day");
+      System.out.println();
+      sunshine();
+      noHome();
+      System.out.println();
+      goesAway();
+      goesAway();
+      goesAway();
+      goesAway();
+      
+   
+   }
+   
+   
+   
+      public static void sunshine() {
+         System.out.println("Ain't no sunshine when she's gone");
+      }
+      
+      public static void goesAway() {
+         System.out.println("Anytime she goes away");
+      }
+      
+      public static void shesGone() {
+         System.out.println("when she's gone");
+      }
+      public static void iKnow() {
+         System.out.println("I know, I know, I know, I know, I know");
+      }
+      public static void noHome() {
+         System.out.println("And this house just ain't no home");
+      }
+
+   
+   
+}
